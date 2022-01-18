83df4138081a10cbdefec94d14f35145fcbc6f7f
NoahAncolie <noahancolie@gmail.com>
2022-01-18 19:15:11 +0100

    ajout cours

---
+++ COURS_RUBY.rb
@@ -0,0 +1,22 @@
+#variable_string = "salut"
+#variable_string2 = "toi"
+
+#variable_integer = 2
+#variable_integer2 = 3
+
+#puts variable_string
+#puts variable_integer
+
+#puts variable_string + variable_string2
+#puts variable_integer + variable_integer2
+#puts variable_string + variable_integer
+
+#array = [1, 2, 3, "salut", "coucou"]
+
+#array = Array.new(10)  
+
+#10.times do |i|
+#   array[i] = "vous êtes arrivés " + i.to_s + " bravo"
+#end
+
+#puts array
